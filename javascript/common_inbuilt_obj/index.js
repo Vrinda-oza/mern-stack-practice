27ac98d59f26f2bf9c1f97c05e98c21944d4c97e
Vrinda Oza <vriza2004@gmail.com>
2026-01-21 22:15:22 +0530

    Complete common in-built objects Practice

---
+++ javascript/common_inbuilt_obj/index.js
@@ -0,0 +1,39 @@
+
+//math obj
+/* console.log(Math.PI);
+
+console.log(Math.max(70,20,100,80,8,9,1,150));
+
+console.log(Math.min(1,0,100,200,750,860,900));
+
+console.log(Math.round(1.6));
+
+console.log(Math.floor(1.4));
+
+console.log(Math.ceil(1.9));
+
+console.log(Math.abs(-51));
+
+console.log(Math.random());
+
+console.log(Math.sqrt(5));
+
+console.log(Math.pow(2,10));
+ */
+
+
+//date and time
+let curr=new Date();
+
+let date=new Date('dec 24 2004 9:30');
+
+let newDate=new Date(2004,11,24,9);
+// console.log(newdate);
+
+// console.log(date);
+
+// console.log(curr);
+
+// console.log(newdate.getFullYear());
+console.log(newDate.setFullYear(2005));
+console.log(newDate);
