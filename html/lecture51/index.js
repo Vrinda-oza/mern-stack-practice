0c39c25e4ddf306be3893d3a6608fa314e3efad5
Vrinda Oza <vriza2004@gmail.com>
2026-01-29 16:30:52 +0530

    Completed DOM manipulation Practice

---
+++ html/lecture51/index.js
@@ -0,0 +1,15 @@
+let mydiv=document.querySelector('#mydiv');
+
+// let newElement=document.createElement('span');
+
+// newElement.textContent="This is Me Vrizaa";
+
+// mydiv.insertAdjacentElement('afterend',newElement);
+
+
+// removal
+let parent = document.querySelector('#mydiv');
+//let parent=child.parentElement;
+let child = document.querySelector('#fpara');
+parent.removeChild(child);
+
